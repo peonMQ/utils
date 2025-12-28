d28690c2e16bc79d18cae6cf3f6394b942d267dd
Aleksander Spro <abspro@gmail.com>
2025-12-28 13:01:56 +0100

    Update enum for dynamic parsing handling

--- enum.lua
+++ enum.lua
@@ -1,54 +1,51 @@
----@class EnumValue
----@field value number
----@field name string
-
 local function Enum(definition)
   local enum = {}
   local reverse = {}
 
+  -- enum value metatable (shared)
+  local ENUM_VALUE_MT = {
+    __tostring = function(self)
+      return self.name
+    end,
+
+    __eq = function(a, b)
+      return a.value == b.value
+    end,
+
+    __newindex = function()
+      error("Enum values are read-only", 2)
+    end,
+  }
+
   for name, value in pairs(definition) do
-    local obj = setmetatable({
+    local obj = {
       value = value,
       name = name,
-    }, {
-      __tostring = function(self)
-        return self.name
-      end,
+      enum = enum,
+    }
 
-      -- allow numeric comparisons
-      __eq = function(a, b)
-        return a.value == b.value
-      end,
+    setmetatable(obj, ENUM_VALUE_MT)
 
-      __lt = function(a, b)
-        return a.value < b.value
-      end,
-
-      __le = function(a, b)
-        return a.value <= b.value
-      end,
-
-      -- allow arithmetic if you really want it
-      __add = function(a, b)
-        return a.value + (type(b) == "number" and b or b.value)
-      end,
-
-      __sub = function(a, b)
-        return a.value - (type(b) == "number" and b or b.value)
-      end,
-    })
-
-    enum[name] = obj
+    rawset(enum, name, obj)
     reverse[value] = obj
   end
 
-  -- lookup by numeric value
+  -- enum container metatable
   return setmetatable(enum, {
+    __enum = true,
+
     __call = function(_, value)
       return reverse[value]
-    end
+    end,
+
+    __newindex = function()
+      error("Enum tables are read-only", 2)
+    end,
+
+    __pairs = function()
+      return pairs(enum)
+    end,
   })
 end
-
 
 return Enum
